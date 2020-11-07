0c0144b37e152e91eec6b1d4d10fba272cb9f97d
Jeremy Puchta <jeremypuchta@outlook.com>
2020-11-07 17:00:59 +0100

    #1 Setup Tailwind.css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
+import React from 'react'
+
 export default function Index() {
-  return <h1>Welcome to leipzigtech</h1>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-extrabold">
+        Welcome to leipzigtech{' '}
+        <span role="img" aria-label="rocket">
+          🚀
+        </span>
+      </h1>
+      <p className="text-xl">
+        A central register of tech companies in Leipzig and the Leipzig area.
+      </p>
+    </div>
+  )
 }
