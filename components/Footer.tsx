3c34f6186db05351e417e8e0fb26904d8c17e7d9
Jeremy Puchta <jeremypuchta@outlook.com>
2020-12-01 17:51:38 +0100

    #21 landing page changes. Header with navigation buttons.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 const Footer = (): JSX.Element => {
   return (
-    <footer className="flex justify-center py-8 px-4 font-mono text-md">
-      <a className="mx-4" href="/">
-        /legal
-      </a>
-      <a className="mx-4" href="/">
-        /privacy
-      </a>
+    <footer className="mt-24 mx-auto py-12 px-4 bg-blue-800 text-sm">
+      <div className="text-center text-white mt-8">
+        <p className="">
+          This site is <span className="font-bold">not</span> tracking your
+          personal information.
+        </p>
+        <p className="pt-2">
+          © 2020 Ali Al-Ali, Fabian Frings, Jeremy Puchta, Fabian Wenzel, Tobias
+          Zschietzschmann. All rights reserved.
+        </p>
+      </div>
     </footer>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,88 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Index(): JSX.Element {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-6xl font-extrabold">
-        Welcome to leipzigtech{' '}
-        <span role="img" aria-label="rocket">
-          🚀
-        </span>
-      </h1>
-      <p className="text-xl">
-        A central register of tech companies in Leipzig and the Leipzig area.
-      </p>
+    <div>
+      <div className="text-center">
+        <h2 className="text-4xl font-extrabold tracking-tight text-gray-900">
+          Find your place in the tech community of Leipzig.
+        </h2>
+        <p className="mt-3 text-lg text-gray-500">
+          A central register of tech companies in Leipzig and the Leipzig area.
+        </p>
+        <div className="mt-5 mb-12 max-w-md mx-auto">
+          <Link href="/companies">
+            <a>
+              <div className="rounded shadow bg-blue-800 text-white py-3 font-semibold">
+                Look for companies
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
+      <div className="mb-4">
+        <h3 className="mt-3 text-2xl font-bold text-center">
+          Explore 100+ tech companies in Leipzig
+        </h3>
+        <section className="text-left">
+          <Link href="#">
+            <a>
+              <div className="flex flex-row rounded shadow-md p-8 my-2">
+                <Image
+                  src="/leipzig-wappen.png"
+                  alt="leipzig wappen"
+                  width={45}
+                  height={50}
+                />
+                <div className="ml-4">
+                  <p className="text-gray-900 font-semibold">
+                    Software Engineer at ABC
+                  </p>
+                  <p className="text-gray-500 text-sm">Address</p>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="#">
+            <a>
+              <div className="flex flex-row rounded shadow-md p-8 my-2">
+                <Image
+                  src="/leipzig-wappen.png"
+                  alt="leipzig wappen"
+                  width={45}
+                  height={50}
+                />
+                <div className="ml-4">
+                  <p className="text-gray-900 font-semibold">
+                    Software Engineer at ABC
+                  </p>
+                  <p className="text-gray-500 text-sm">Address</p>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="#">
+            <a>
+              <div className="flex flex-row rounded shadow-md p-8 my-2">
+                <Image
+                  src="/leipzig-wappen.png"
+                  alt="leipzig wappen"
+                  width={45}
+                  height={50}
+                />
+                <div className="ml-4">
+                  <p className="text-gray-900 font-semibold">
+                    Software Engineer at ABC
+                  </p>
+                  <p className="text-gray-500 text-sm">Address</p>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </section>
+      </div>
     </div>
   )
 }
