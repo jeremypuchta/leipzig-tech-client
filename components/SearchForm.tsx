f0f34f41f736e68471cad2aa8f6a1bdaaa7997b7
Jeremy Puchta <jeremypuchta@outlook.com>
2021-01-08 18:09:19 +0100

    #27 Create formik form with company name filter

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,64 +1,36 @@
 import React, { SyntheticEvent, useState } from 'react'
+import { useFormik } from 'formik'
 
-interface District {
-  value: string
-  label: string
-}
 export default function SearchForm(setFilteredCompanies: any): JSX.Element {
-  const [name, setName] = useState<string>('')
-  const [selectedDistricts, setSelectedDistricts] = useState<District[]>([])
-
-  const handleCompanyNameChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setName(event.target.value)
-  }
-
-  const handleDistrictSelectionChange = (
-    value: ValueType<District, boolean>
-  ) => {
-    setSelectedDistricts(value as District[])
-  }
-
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
-  }
-
-  const options: District[] = [
-    { value: 'suedvorstadt', label: 'Südvorstadt' },
-    { value: 'connewitz', label: 'Connewitz' },
-    { value: 'plagwitz', label: 'Plagwitz' },
-    { value: 'lindenau', label: 'Lindenau' },
-    { value: 'zentrum-west', label: 'Zentrum West' },
-    { value: 'zentrum-sued', label: 'Zentrum Süd' },
-  ]
-
-  const customStyles = {
-    control: (provided: any, state: any) => ({
-      ...provided,
-      borderRadius: '0.5rem',
-      borderColor: state.selectProps.borderColor,
-    }),
-  }
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    },
+  })
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex max-w-5xl rounded shadow p-4 mb-8 mx-auto justify-between">
-        <input
-          className="w-full border rounded-lg pl-2 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent placeholder-gray-900"
-          type="text"
-          value={name}
-          onChange={handleCompanyNameChange}
-          name="name"
-          placeholder="Company Name"
-        />
-        <button
-          type="submit"
-          className="py-2 px-4 rounded-lg shadow-lg bg-blue-800 text-white"
-        >
-          Search
-        </button>
-      </div>
+    <form
+      onSubmit={formik.handleSubmit}
+      className="flex shadow p-4 m-4 justify-between items-center"
+    >
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+        placeholder="Company Name"
+        className="w-11/12 p-2 rounded border border-blue-400 focus:border-2"
+      />
+      <button
+        type="submit"
+        className="px-4 py-2 rounded shadow text-black hover:bg-blue-800 hover:text-white"
+      >
+        Submit
+      </button>
     </form>
   )
 }
