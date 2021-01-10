50b539d4e04a40e1ca48be06f7c30e74f9a03ef5
Jeremy Puchta <jeremypuchta@outlook.com>
2021-01-10 18:22:57 +0100

    27 Introduce Formik and implement call to API

--- pages/companies.tsx
+++ pages/companies.tsx
@@ -4,9 +4,9 @@ import dynamic from 'next/dynamic'
 import React, { useState } from 'react'
 
 import * as faker from 'faker'
+import { Formik } from 'formik'
 import CompanyCardList from '../components/CompanyCardList'
 import { Company } from '../models/Company.model'
-import SearchForm from '../components/SearchForm'
 
 const LocationMap = dynamic(() => import('../components/Map'), {
   ssr: false,
@@ -25,7 +25,45 @@ const CompaniesPage = ({
 
   return (
     <div>
-      <SearchForm />
+      <Formik
+        initialValues={{ name: '' }}
+        onSubmit={async (values) => {
+          const res = await axios.get(`${process.env.BASE_API_URL}/companies`, {
+            params: {
+              name: values.name,
+            },
+          })
+
+          {
+            /*
+           TODO: Calling the API works and the response is correct as well (see Network Tab in Browser). How to update the list of companies?
+           */
+          }
+        }}
+      >
+        {({ values, handleSubmit, handleChange }) => (
+          <form
+            onSubmit={handleSubmit}
+            className="flex shadow p-4 m-4 justify-between items-center"
+          >
+            <input
+              id="name"
+              name="name"
+              type="text"
+              onChange={handleChange}
+              value={values.name}
+              placeholder="Company Name"
+              className="w-11/12 p-2 rounded border border-blue-400 focus:border-2"
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 rounded shadow text-black hover:bg-blue-800 hover:text-white"
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
       <div className="flex h-content">
         <div className="w-full h-full overflow-auto">
           <CompanyCardList
@@ -40,6 +78,7 @@ const CompaniesPage = ({
     </div>
   )
 }
+
 export default CompaniesPage
 
 export const getServerSideProps: GetServerSideProps<{
