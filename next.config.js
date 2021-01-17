405c1cc701786751b3b43d97ad7882f7be67dddb
Fabian Wenzel <fw12rofy@studserv.uni-leipzig.de>
2021-01-17 16:28:21 +0100

    added auth and env handling

--- next.config.js
+++ next.config.js
@@ -3,12 +3,18 @@ function getEnv() {
     return {
       PRODUCTION: false,
       BASE_API_URL: 'http://localhost:8080',
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
+      GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+      JAWG_TOKEN: process.env.JAWG_TOKEN,
     }
   }
   // edit BASE_API_URL when deployed
   return {
     PRODUCTION: true,
     BASE_API_URL: 'http://localhost:8080',
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    JAWG_TOKEN: process.env.JAWG_TOKEN,
   }
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import type { AppProps } from 'next/app'
-
 import '../styles/globals.css'
+
+import { SSRCookies, SSRKeycloakProvider } from '@react-keycloak/ssr'
+import cookie from 'cookie'
+import { IncomingMessage } from 'http'
+import { AppContext, AppProps } from 'next/app'
+import React from 'react'
+
 import Layout from '../components/Layout'
 
-function App({ Component, pageProps }: AppProps): JSX.Element {
+const keycloakCfg = {
+  url: 'http://localhost:8083/auth',
+  realm: 'baeldung',
+  clientId: 'jwtClient',
+}
+
+interface InitialProps {
+  cookies: unknown
+}
+
+function App({
+  Component,
+  pageProps,
+  cookies,
+}: AppProps & InitialProps): JSX.Element {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SSRKeycloakProvider
+      keycloakConfig={keycloakCfg}
+      persistor={SSRCookies(cookies)}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SSRKeycloakProvider>
   )
 }
 
+function parseCookies(req?: IncomingMessage) {
+  if (!req || !req.headers) {
+    return {}
+  }
+  return cookie.parse(req.headers.cookie || '')
+}
+
+export const getInitialProps = async (context: AppContext) => {
+  return {
+    cookies: parseCookies(context?.ctx?.req),
+  }
+}
 export default App
