c628311eea9b211aee3be99d6078b6c0b00bb6ac
Jeremy Puchta <jeremypuchta@outlook.com>
2021-01-20 21:52:38 +0100

    #54 Implementation of filter options by sectors and districts

--- components/SelectField.tsx
+++ components/SelectField.tsx
@@ -1,30 +1,40 @@
+import React from 'react'
 import { useField, FieldProps } from 'formik'
 import Select, { Option, ReactSelectProps } from 'react-select'
 
 interface PropsType {
   [x: string]: any
-
   name: string
+  placeholder: string
 }
 
 const SelectField: React.FC<ReactSelectProps & FieldProps> = ({
   ...props
 }: PropsType) => {
   const [field, meta, helpers] = useField(props.name)
-
-  const { options } = props
-  const { touched, error, value } = meta
+  const { className, options, placeholder, iid } = props
   const { setValue } = helpers
+
+  const style = {
+    control: (base) => ({
+      ...base,
+      border: 0,
+      boxShadow: 'none',
+    }),
+  }
 
   return (
     <Select
+      className={className}
       isMulti
       options={props.options}
       name={field.name}
       onChange={(option: Option) =>
         setValue(option ? option.map((s) => s.value) : [])
       }
-      instanceId={props.iid}
+      instanceId={iid}
+      placeholder={placeholder}
+      styles={style}
     />
   )
 }
