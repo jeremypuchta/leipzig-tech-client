447b354d3b8a40daea911d622e9ca79628384ebc
Jeremy Puchta <jeremypuchta@outlook.com>
2021-01-27 15:47:09 +0100

    #63 Change utility classes to make Statistics component responsive. Also slight changes in color and padding.

--- components/StatisticEntry.tsx
+++ components/StatisticEntry.tsx
@@ -11,10 +11,12 @@ export default function StatisticEntry({
   return (
     <div className="flex flex-col h-full place-content-around">
       <div className="flex flex-col">
-        <div className="text-blue-500 font-bold text-6xl mb-3">
+        <div className="text-blue-800 font-bold text-5xl mb-3 lg:text-6xl">
           <Counter value={statistic.value} duration={statistic.duration} />
         </div>
-        <div className="text-gray-400 text-2xl">{statistic.title}</div>
+        <div className="text-gray-500 text-lg lg:text-2xl">
+          {statistic.title}
+        </div>
       </div>
     </div>
   )
